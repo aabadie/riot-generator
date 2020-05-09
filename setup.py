94efacc64815a715f9d48a2b91a22cb453a238df
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-05-09 21:22:46 +0200

    setup.py: update supported Python versions in classifiers

--- setup.py
+++ setup.py
@@ -46,10 +46,10 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Utilities',
     ],
 
