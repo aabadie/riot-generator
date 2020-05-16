37fe4f938a259325961ef7304b04dce445534a84
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-05-16 11:38:53 +0200

    riotgen/tests: add board include expected files

---
+++ riotgen/tests/test_data/board/board.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2020 test_orga
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_test
+ * @{
+ *
+ * @file
+ * @brief       Board specific definitions for the Test
+ *
+ * @author      test_name <test_email>
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "cpu.h"
+#include "periph_conf.h"
+#include "periph_cpu.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Put here board specific definitions:
+ - LEDs/buttons macros,
+ - on-board drivers parameters
+ - xtimer configuration
+ */
+
+/**
+ * @brief   Initialize board specific hardware
+ */
+void board_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H */
+/** @} */
