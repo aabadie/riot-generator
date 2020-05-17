0d52c84c1616f57aa9cf358d693d8feed056f163
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-05-17 21:06:53 +0200

    riotgen/main: factorize subcommand option decorators

--- riotgen/main.py
+++ riotgen/main.py
@@ -13,114 +13,73 @@ from .test import generate_test
 from . import __version__
 
 
+class SharedCommand(click.core.Command):
+    """Class for shared subcommand options"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        options = [
+            click.core.Option(
+                ("-i", "--interactive",), is_flag=True, help="Use interactive mode"
+            ),
+            click.core.Option(
+                ("-c", "--config",),
+                type=click.File(mode="r"),
+                help="Use a configuration file",
+            ),
+            click.core.Option(
+                ("-r", "--riotbase"),
+                type=click.Path(exists=True),
+                default=os.getenv("RIOTBASE"),
+            ),
+        ]
+        self.params += options
+
+
 @click.group()
 @click.version_option(version=__version__)
 def riotgen():  # pylint:disable=missing-function-docstring
     pass
 
 
-@riotgen.command(help="Bootstrap a RIOT application")
-@click.option("-d", "--output_dir", type=click.Path(exists=True), default=os.getcwd())
-@click.option("-i", "--interactive", is_flag=True, help="Use interactive mode")
-@click.option(
-    "-c",
-    "--config",
-    type=click.File(mode="r"),
-    help="Configuration file for application",
-)
-@click.option(
-    "-r", "--riotbase", type=click.Path(exists=True), default=os.getenv("RIOTBASE")
-)
+@riotgen.command(cls=SharedCommand, help="Bootstrap a RIOT application")
+@click.option("-d", "--output-dir", type=click.Path(exists=True), default=os.getcwd())
 def application(output_dir, interactive, config, riotbase):
     """Entry point for application subcommand."""
     generate_application(output_dir, interactive, config, riotbase)
 
 
-@riotgen.command(help="Bootstrap a RIOT board support")
-@click.option("-i", "--interactive", is_flag=True, help="Use interactive mode")
-@click.option(
-    "-c", "--config", type=click.File(mode="r"), help="Configuration file for board"
-)
-@click.option(
-    "-r", "--riotbase", type=click.Path(exists=True), default=os.getenv("RIOTBASE")
-)
+@riotgen.command(cls=SharedCommand, help="Bootstrap a RIOT board support")
 def board(interactive, config, riotbase):
     """Entry point for board subcommand."""
     generate_board(interactive, config, riotbase)
 
 
-@riotgen.command(help="Bootstrap a RIOT driver module")
-@click.option("-i", "--interactive", is_flag=True, help="Use interactive mode")
-@click.option(
-    "-c",
-    "--config",
-    type=click.File(mode="r"),
-    help="Configuration file for the driver module",
-)
-@click.option(
-    "-r", "--riotbase", type=click.Path(exists=True), default=os.getenv("RIOTBASE")
-)
+@riotgen.command(cls=SharedCommand, help="Bootstrap a RIOT driver module")
 def driver(interactive, config, riotbase):
     """Entry point for driver subcommand."""
     generate_driver(interactive, config, riotbase)
 
 
-@riotgen.command(help="Bootstrap a RIOT example application")
-@click.option("-i", "--interactive", is_flag=True, help="Use interactive mode")
-@click.option(
-    "-c",
-    "--config",
-    type=click.File(mode="r"),
-    help="Configuration file for example application",
-)
-@click.option(
-    "-r", "--riotbase", type=click.Path(exists=True), default=os.getenv("RIOTBASE")
-)
+@riotgen.command(cls=SharedCommand, help="Bootstrap a RIOT example application")
 def example(interactive, config, riotbase):
     """Entry point for example application subcommand."""
     generate_example(interactive, config, riotbase)
 
 
-@riotgen.command(help="Bootstrap a RIOT system module")
-@click.option("-i", "--interactive", is_flag=True, help="Use interactive mode")
-@click.option(
-    "-c",
-    "--config",
-    type=click.File(mode="r"),
-    help="Configuration file for the system module",
-)
-@click.option(
-    "-r", "--riotbase", type=click.Path(exists=True), default=os.getenv("RIOTBASE")
-)
+@riotgen.command(cls=SharedCommand, help="Bootstrap a RIOT system module")
 def module(interactive, config, riotbase):
     """Entry point for module subcommand."""
     generate_module(interactive, config, riotbase)
 
 
-@riotgen.command(help="Bootstrap a RIOT external package")
-@click.option("-i", "--interactive", is_flag=True, help="Use interactive mode")
-@click.option(
-    "-c", "--config", type=click.File(mode="r"), help="Configuration file for package"
-)
-@click.option(
-    "-r", "--riotbase", type=click.Path(exists=True), default=os.getenv("RIOTBASE")
-)
+@riotgen.command(cls=SharedCommand, help="Bootstrap a RIOT external package")
 def pkg(interactive, config, riotbase):
     """Entry point for pkg subcommand."""
     generate_pkg(interactive, config, riotbase)
 
 
-@riotgen.command(help="Bootstrap a RIOT test application")
-@click.option("-i", "--interactive", is_flag=True, help="Use interactive mode")
-@click.option(
-    "-c",
-    "--config",
-    type=click.File(mode="r"),
-    help="Configuration file for test application",
-)
-@click.option(
-    "-r", "--riotbase", type=click.Path(exists=True), default=os.getenv("RIOTBASE")
-)
+@riotgen.command(cls=SharedCommand, help="Bootstrap a RIOT test application")
 def test(interactive, config, riotbase):
     """Entry point for test subcommand."""
     generate_test(interactive, config, riotbase)
