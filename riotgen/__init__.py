e628562f68f0a2578c8e88a393e6729b36b3c69a
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-05-17 12:03:19 +0200

    bump to version 0.4.0

--- riotgen/__init__.py
+++ riotgen/__init__.py
@@ -4,10 +4,12 @@ It can be used to bootstrap:
 - a new board support
 - a new external application
 - a new RIOT example application
+- a new RIOT driver
+- a new RIOT system module
 - a new test application
 - a new package for RIOT
 """
 
-__version__ = "0.3.2-dev"
+__version__ = "0.4.0"
 
 from .main import riotgen  # noqa
