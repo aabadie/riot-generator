ce90f0b4fec1dab81fec81636210c2ea1bfc1cdc
Alexandre Abadie <alexandre.abadie@inria.fr>
2023-03-07 15:13:13 +0100

    test_data: update year

--- riotgen/tests/test_data/driver/driver.c
+++ riotgen/tests/test_data/driver/driver.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 test_orga
+ * Copyright (C) 2023 test_orga
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
