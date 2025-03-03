ff921855e935db23c5d7288a83428a3ca7aa1560
Alexandre Abadie <alexandre.abadie@inria.fr>
2025-03-03 14:51:06 +0100

    riotgen: restore module file

---
+++ riotgen/__init__.py
@@ -0,0 +1,11 @@
+"""RIOT generator is a helper library for bootstraping RIOT code.
+
+It can be used to bootstrap:
+- a new board support
+- a new external application
+- a new RIOT example application
+- a new RIOT driver
+- a new RIOT system module
+- a new test application
+- a new package for RIOT
+"""
